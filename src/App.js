633d8b825da895acad6bf9f74f8774b6b1aa1fa6
akshpnwr <akashpanwar9976@gmail.com>
2021-03-09 22:02:40 +0530

    CSS modules implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import classes from "./App.module.css";
+import { Component } from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className={classes.Red}>akash</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
